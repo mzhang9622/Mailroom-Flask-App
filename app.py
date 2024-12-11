339ea67ca1d7816c1ee2f049302b9a98eb9a1e02
Jordan Harris Smith <jhsmit25@colby.edu>
2024-12-11 14:39:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ app.py
 '''
 
 from website import create_app
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if os.environ.get('CONFIG_TYPE') == "PRODUCTION":
+        app.run(debug=False)
+    else:
+        app.run(debug=True)
