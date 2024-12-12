6c9373f08e2855139d61b8a03e5340eb5983a837
Ming Zhang <mzhang9622@gmail.com>
2024-12-12 15:02:28 -0500

    Changing keys for Jawsdb

--- website/__init__.py
+++ website/__init__.py
@@ -24,7 +24,8 @@ def create_app():
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
     else:
         app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///maildatabase.db'
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('JAWSDB_URL')
+        # 'sqlite:///maildatabase.db'
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
@@ -33,7 +34,7 @@ def create_app():
     login_manager.login_view = 'auth.login'
 
     from .models import User
-    from .views import main_blueprint
+    from .views import main_blueprint 
     from .auth import auth_blueprint
     
     @login_manager.user_loader
